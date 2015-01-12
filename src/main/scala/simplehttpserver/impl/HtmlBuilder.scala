14e0eb3cc28a4365544dad14eaac49df64fe9fef
amaya <s@pphire.net>
2015-01-12 15:50:46 +0900

    テンプレートエンジンにデフォルト値を設定できるように

--- src/main/scala/simplehttpserver/impl/HtmlBuilder.scala
+++ src/main/scala/simplehttpserver/impl/HtmlBuilder.scala
@@ -3,14 +3,13 @@ package simplehttpserver.impl
 import scala.annotation.tailrec
 
 //超簡易的なテンプレートエンジン
-//一切エスケープを考慮していないので注意
 class HtmlBuilder {
   def buildHtml(template: String)(patterns: (String, String)*): String = {
 
     @tailrec
     def go(pat: Seq[(String, String)], acc: String): String = {
       if (pat.size == 0)
-        acc
+        acc.replaceAll( """@\w+?\(\((.+?)\)\)""", "$1")
       else
         go(pat.tail, acc.replaceAll("(?<!@)@" + pat.head._1,
           pat.head._2.replaceAll("@", "@@")))
